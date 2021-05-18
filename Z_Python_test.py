f641d1da67b909fd89e0392b947296789ca01710
anyboycho <this9611@chol.com>
2021-05-18 15:45:35 +0900

    first commit

---
+++ Z_Python_test.py
@@ -0,0 +1,16 @@
+
+
+
+multi_line_string = """this is line number 1
+this is line number 2
+this is line number 3
+this is line number 4"""
+
+print(multi_line_string)
+
+multi_line_string = ("this is line number 1 "
+"this is line number 2 "
+"this is line number 3 "
+"this is line number 4")
+
+print(multi_line_string)
